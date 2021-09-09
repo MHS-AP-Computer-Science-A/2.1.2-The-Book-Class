3b74008800ce8a27e5edf355bb6b5eb96191e567
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-09-09 16:04:32 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		//Add code to the Book class so that the main method runs as intented
 		Book b = new Book("The Great Avocado", "F. Scott Guacamole", 243);
-		b.daysToRead = 14;
 		b.printBook();		
 	}
 
