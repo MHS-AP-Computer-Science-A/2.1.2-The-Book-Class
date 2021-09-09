8f46da713fcc0b7174dea90f4d194bf2bdf85ae5
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-09-09 15:28:40 -0400

    Add files via upload

---
+++ src/Book.java
@@ -0,0 +1,17 @@
+//Write your Book class here
+public class Book {
+	//Put the instance variables here
+	
+	
+	//Fill in the constructor method
+	public Book(String t, String a, int p) {
+		
+	}
+	
+	//Fill in the printBook method
+	//This should just be println statements
+	public void printBook() {
+		
+	}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Add code to the Book class so that the main method runs as intented
+		Book b = new Book("The Great Avocado", "F. Scott Guacamole", 243);
+		b.daysToRead = 14;
+		b.printBook();		
+	}
+
+}
